e1d8198112b8e8c5a9301bf529884c8f623ee1d0
Natanael Fonseca <natanael.vaz@gmail.com>
2018-12-26 21:23:54 -0400

    colocando certificado digital no servidor de configuracao

--- docker/run.sh
+++ docker/run.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 
 echo "********************************************************"
-echo "Waiting for the eureka server to start on port 8761"
+echo "Esperando pelo servidor eureka na porta $EUREKASERVER_PORT"
 echo "********************************************************"
-while ! `nc -z servicediscovery 8761`; do sleep 3; done
-echo "******* Eureka Server has started"
+while ! `nc -z eurekaserver  $EUREKASERVER_PORT`; do sleep 3; done
+echo "******* Servidor eureka iniciou"
 
-java -jar /usr/local/@project.artifactId@/@project.build.finalName@.jar
+echo "********************************************************"
+echo "Iniciando Servidor de Configuracao"
+echo "********************************************************"
+
+java -Djava.security.egd=file:/dev/./urandom -Dserver.port=$PORT  \
+     -Deureka.client.serviceUrl.defaultZone=$EUREKA_DEFAULT_ZONE  \
+     -Dspring.profiles.active=$PROFILE                            \
+     -jar /usr/local/@project.artifactId@/@project.build.finalName@.jar
